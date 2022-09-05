5263a4a80e3bd4f7dafa20e622d0c0350a2527c5
Santiago Acevedo <sacevedor@unbosque.edu.co>
2022-09-05 16:36:02 -0500

    haciendo mas vainas en daos

--- src/co/edu/unbosque/model/Conexion.java
+++ src/co/edu/unbosque/model/Conexion.java
@@ -4,13 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Conexion {
+	private Connection connection = null;
 	
 	public Conexion() {
 		
 	}
 	
 	public void abrirConexion() {
-		Connection connection = null;
+		
 		 
 	      try{
 	         connection = DriverManager.getConnection( "jdbc:sqlite:Data\\data.db" );
@@ -22,6 +23,20 @@ public class Conexion {
 	         System.err.println( ex.getClass().getName() + ": " + ex.getMessage() );
 	         System.out.println("Error en la conexión");
 	      }
+	}
+
+	/**
+	 * @return the connection
+	 */
+	public Connection getConnection() {
+		return connection;
+	}
+
+	/**
+	 * @param connection the connection to set
+	 */
+	public void setConnection(Connection connection) {
+		this.connection = connection;
 	}
 
 }
--- src/co/edu/unbosque/model/persistence/PersonaSqlDAO.java
+++ src/co/edu/unbosque/model/persistence/PersonaSqlDAO.java
@@ -1,5 +1,41 @@
 package co.edu.unbosque.model.persistence;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+import co.edu.unbosque.model.Conexion;
+
 public class PersonaSqlDAO {
+
+	private static Conexion connection;
+
+	public PersonaSqlDAO(Conexion connection) {
+
+		this.connection = connection;
+	}
+
+	public void insertarPersonaDB(PersonaDTO persona) {
+		connection.abrirConexion();
+		PreparedStatement stmt = null;
+		try {
+			String sql = "INSERT INTO persona (cedula, nombre, apellido, sexo, edad, telefono) "
+					+ "VALUES (?, ?, ?, ?, ?, ?)";
+			stmt = connection.getConnection().prepareStatement(sql);
+			stmt.setString(1, persona.getCedula());
+			stmt.setString(2, persona.getNombre());
+			stmt.setString(3, persona.getApellido());
+			stmt.setString(4, persona.getSexo());
+			stmt.setString(5, persona.getEdad());
+			stmt.setString(6, persona.getTelefono());
+			stmt.executeUpdate(sql);
+			stmt.close();
+			connection.getConnection().commit();
+			connection.getConnection().close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Records created successfully");
+	}
 
 }
